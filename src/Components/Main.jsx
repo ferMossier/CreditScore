f012318a2d3aaa96da0cfe18b7a7ddda1aa1a272
FerMossier <mossierfernando@gmail.com>
2023-04-01 21:14:50 -0300

    init

---
+++ src/Components/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Data from "./Data";
+
+const Main = () => {
+  return (
+    <div>
+      <Data />
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/Services/Inference.js
@@ -0,0 +1,33 @@
+import AWS from "aws-sdk";
+import SageMakerRuntime from "aws-sdk/clients/sagemakerruntime";
+
+export const Inference = async (features) => {
+  AWS.config.update({
+    accessKeyId: "AKIAZ2OXW5ABALSW7HIR",
+    secretAccessKey: "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",
+    region: "sa-east-1",
+  });
+
+  const sagemaker = new SageMakerRuntime();
+
+  const body = {
+    instances: [
+      {
+        features: features,
+      },
+    ],
+  };
+
+  try {
+    const respuesta = await sagemaker
+      .invokeEndpoint({
+        EndpointName: "linear-learner-2023-03-25-19-51-44-328",
+        ContentType: "application/json",
+        Body: JSON.stringify(body),
+      })
+      .promise();
+    return respuesta.Body.toString("utf-8");
+  } catch (error) {
+    console.log(error);
+  }
+};
